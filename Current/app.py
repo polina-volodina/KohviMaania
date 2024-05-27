a9c7396862f5520a825bcecff4dc3462b76569bd
amadnaa <amanda.vaarmann@gmail.com>
2024-05-27 19:54:18 +0300

    Buy ei tööta veel

--- Current/app.py
+++ Current/app.py
@@ -3,19 +3,16 @@ from product import Product
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates', static_folder='images')
-app = Flask(__name__, static_folder='images')
-app = Flask(__name__)
 
-@app.route("/")
 # Initialize money and coffee count
 money = 0
 coffee_count = 0
 
 @app.route('/')
 def index():
-    return render_template("index.html")
     global money, coffee_count
     return render_template('index.html', money=money, coffee_count=coffee_count)
+
 
 @app.route('/buy_coffee', methods=['POST'])
 def buy_coffee():
@@ -33,5 +30,4 @@ def increase_money():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(debug=True)
