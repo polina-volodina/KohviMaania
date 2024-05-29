86330007b6ae54c506d956e7f0634584f01564f0
amadnaa <amanda.vaarmann@gmail.com>
2024-05-29 22:29:14 +0300

    Drag and drop works properly

--- Current/app.py
+++ Current/app.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template
-from product import Product
+#from product import Product
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates', static_folder='images')
 
 # Initialize money and coffee count
 money = 0
-coffee_count = 0
+coffeeCount = 5
 
 @app.route('/')
 def index():
     global money, coffee_count
-    return render_template('index.html', money=money, coffee_count=coffee_count)
+    return render_template('index.html', money=money, coffeeCount=coffeeCount)
 
-
-"""
 @app.route('/buy_coffee', methods=['POST'])
 def buy_coffee():
     global money, coffee_count
     coffee_price = 10
     if money >= coffee_price:
-        money -= coffee_price
         coffee_count += 1
     return redirect(url_for('index'))
-
+"""
 @app.route('/increase_money', methods=['POST'])
 def increase_money():
     global money
     money += 1
     return redirect(url_for('index'))
+
 """
 
 if __name__ == '__main__':
