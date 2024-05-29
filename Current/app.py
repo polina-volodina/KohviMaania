bd5ef6bb2646ffb41591ff44739d2721ec6ba224
amadnaa <amanda.vaarmann@gmail.com>
2024-05-29 23:03:10 +0300

    JavaScript for loop not working
    AppendChild

--- Current/app.py
+++ Current/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__, template_folder='templates', static_folder='images')
 
 # Initialize money and coffee count
 money = 0
-coffeeCount = 5
+
 
 @app.route('/')
 def index():
     global money, coffee_count
-    return render_template('index.html', money=money, coffeeCount=coffeeCount)
+    return render_template('index.html', money=money)
 
+"""
 @app.route('/buy_coffee', methods=['POST'])
 def buy_coffee():
     global money, coffee_count
@@ -19,7 +20,7 @@ def buy_coffee():
     if money >= coffee_price:
         coffee_count += 1
     return redirect(url_for('index'))
-"""
+
 @app.route('/increase_money', methods=['POST'])
 def increase_money():
     global money
